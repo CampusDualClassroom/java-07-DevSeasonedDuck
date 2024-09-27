e2341e086344da37013c3ff5ba54d2b792fa8c05
Diego Alonso Cárcamo Gutiérrez <diegocarcamo.contacto@gmail.com>
2024-09-27 12:28:13 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise07.java
+++ src/main/java/com/campusdual/classroom/Exercise07.java
@@ -1,9 +1,17 @@
 package com.campusdual.classroom;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise07 {
 
     public static void main(String[] args) {
+        int num=10;
+
+        positionInAList(num);
+        sumFirstNaturalNumbers(num);
+        showFirstNaturalNumbers(num);
 
     }
 
@@ -14,19 +22,48 @@ public class Exercise07 {
     // Si lo encuentra, que imprima el siguiente mensaje → El elemento X se encuentra en la posición: Y
     // Si NO lo encuentra, que imprima el siguiente mensaje → El elemento X no se encuentra en la lista.
     public static void positionInAList(int num) {
+        List<Integer>list = new ArrayList<>();
+        boolean isIn = false;
+        int index = 0;
+        for (int i=0;  i<11; i++){
+            list.add(i);
+        }
+        for (int i=0; i<11; i++){
+            if(list.get(i)==num){
+                isIn = true;
+                index = i;
+                break;
+            }
+            else
+                isIn = false;
+        }
+        System.out.println(list.get(index));
+        System.out.println(isIn);
+        if(isIn){
+            System.out.println(num+" se encuentra en la posición: "+list.indexOf(index));
+        }else
+            System.out.println(num+" no se encuentra en la lista.");
 
     }
 
     //TODO ↓
     // Que imprima como mensaje la suma de los primeros N números positivos
     public static void sumFirstNaturalNumbers(int num) {
+        int sum=0, nnum=1;
+        for (int i=0; i<num; i++){
+            sum=sum+nnum;
+            nnum=nnum+1;
+        }
 
+        System.out.println(sum);
     }
 
     //TODO ↓
     // Que imprima por pantalla los N primeros números positivos
     public static void showFirstNaturalNumbers(int num) {
-
+        for (int i=0; i<=num; i++){
+            System.out.println(i+1);
+        }
     }
 
 }
